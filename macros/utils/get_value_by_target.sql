ed05e10107223392d67ca4b858c38437211f0a13
bill-warner <71764352+bill-warner@users.noreply.github.com>
2021-09-21 12:33:09 +0300

    Add dynamic variables based on target name (Close #28)

---
+++ macros/utils/get_value_by_target.sql
@@ -0,0 +1,11 @@
+{% macro get_value_by_target(dev_value, default_value, dev_target_name='dev') %}
+
+  {% if target.name == dev_target_name %}
+    {% set value = dev_value %}
+  {% else %}
+    {% set value = default_value %}
+  {% endif %}
+
+  {{ return(value) }}
+
+{% endmacro %}
