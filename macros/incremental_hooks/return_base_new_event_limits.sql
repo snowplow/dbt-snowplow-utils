4562c185b8e53efee65188b26b7ec99a89b54baa
bill-warner <71764352+bill-warner@users.noreply.github.com>
2021-12-01 15:45:47 +0200

    Fix for unexpired sessions (Close #48)

--- macros/incremental_hooks/return_base_new_event_limits.sql
+++ macros/incremental_hooks/return_base_new_event_limits.sql
@@ -1,7 +1,7 @@
 {% macro return_base_new_event_limits(base_events_this_run) -%}
 
   {% if not execute %}
-    {{ return(['','',''])}}
+    {{ return(['','','',''])}}
   {% endif %}
   
   {% set limit_query %} 
@@ -9,8 +9,11 @@
       lower_limit, 
       upper_limit,
       {{ snowplow_utils.timestamp_add('day', 
+                                     -var("snowplow__session_lookback_days", 365),
+                                     'lower_limit') }} as session_lookback_limit,
+      {{ snowplow_utils.timestamp_add('day', 
                                      -var("snowplow__max_session_days", 3),
-                                     'lower_limit') }} as session_lookback_limit
+                                     'lower_limit') }} as session_start_limit
 
     from {{ base_events_this_run }} 
     {% endset %}
@@ -22,8 +25,9 @@
     {% set lower_limit = snowplow_utils.cast_to_tstamp(results.columns[0].values()[0]) %}
     {% set upper_limit = snowplow_utils.cast_to_tstamp(results.columns[1].values()[0]) %}
     {% set session_lookback_limit = snowplow_utils.cast_to_tstamp(results.columns[2].values()[0]) %}
+    {% set session_start_limit = snowplow_utils.cast_to_tstamp(results.columns[3].values()[0]) %}
 
-  {{ return([lower_limit, upper_limit, session_lookback_limit]) }}
+  {{ return([lower_limit, upper_limit, session_lookback_limit, session_start_limit]) }}
 
   {% endif %}
 {%- endmacro %}
