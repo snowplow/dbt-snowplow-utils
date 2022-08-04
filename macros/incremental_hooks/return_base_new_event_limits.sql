3bc838dae1cbb0485af68edd6ee7be5445e8b521
georgewoodhead <50772749+georgewoodhead@users.noreply.github.com>
2022-08-04 09:31:03 +0100

    Fix initial dbt compile error (Close #69)

--- macros/incremental_hooks/return_base_new_event_limits.sql
+++ macros/incremental_hooks/return_base_new_event_limits.sql
@@ -1,29 +1,48 @@
 {% macro return_base_new_event_limits(base_events_this_run) -%}
 
+  {# In case of not execute just return empty strings to avoid hitting database #}
   {% if not execute %}
     {{ return(['','',''])}}
   {% endif %}
-  
-  {% set limit_query %} 
-    select 
-      lower_limit, 
-      upper_limit,
-      {{ snowplow_utils.timestamp_add('day', 
-                                     -var("snowplow__max_session_days", 3),
-                                     'lower_limit') }} as session_start_limit
 
-    from {{ base_events_this_run }} 
+  {% set target_relation = adapter.get_relation(
+        database=base_events_this_run.database,
+        schema=base_events_this_run.schema,
+        identifier=base_events_this_run.name) %}
+
+  {% if target_relation is not none %}
+
+    {% set limit_query %}
+      select
+        lower_limit,
+        upper_limit,
+        {{ snowplow_utils.timestamp_add('day',
+                                      -var("snowplow__max_session_days", 3),
+                                      'lower_limit') }} as session_start_limit
+
+      from {{ base_events_this_run }}
     {% endset %}
 
-  {% set results = run_query(limit_query) %}
-   
-  {% if execute %}
+    {% set results = run_query(limit_query) %}
 
-    {% set lower_limit = snowplow_utils.cast_to_tstamp(results.columns[0].values()[0]) %}
-    {% set upper_limit = snowplow_utils.cast_to_tstamp(results.columns[1].values()[0]) %}
-    {% set session_start_limit = snowplow_utils.cast_to_tstamp(results.columns[2].values()[0]) %}
+    {% if execute %}
 
-  {{ return([lower_limit, upper_limit, session_start_limit]) }}
+      {% set lower_limit = snowplow_utils.cast_to_tstamp(results.columns[0].values()[0]) %}
+      {% set upper_limit = snowplow_utils.cast_to_tstamp(results.columns[1].values()[0]) %}
+      {% set session_start_limit = snowplow_utils.cast_to_tstamp(results.columns[2].values()[0]) %}
+
+      {{ return([lower_limit, upper_limit, session_start_limit]) }}
+
+    {% endif %}
+
+  {% else %}
+
+    {% do exceptions.warn("Snowplow Warning: " ~ base_events_this_run ~ " does not exist. This is expected if you are compiling a fresh installation of the dbt-snowplow-* packages.") %}
+
+    {% set dummy_limit = snowplow_utils.cast_to_tstamp('9999-01-01 00:00:00') %}
+
+    {{ return([dummy_limit, dummy_limit, dummy_limit]) }}
 
   {% endif %}
+
 {%- endmacro %}
