240b792bb8d3e9910eb64c2864fa28ca956e9ce6
emielver <emiel@snowplowanalytics.com>
2022-10-26 10:21:56 +0100

    Bump `dbt-utils` version for patch fix (Close #92)

--- integration_tests/models/materializations/default_strategy/test_snowplow_incremental.sql
+++ integration_tests/models/materializations/default_strategy/test_snowplow_incremental.sql
@@ -1,9 +1,9 @@
-{# Tests both RS/PG (delete/insert) and BQ/Snowflake/Databricks (merge) snowplow_incremental materialization 
+{# Tests both RS/PG (delete/insert) and BQ/Snowflake/Databricks (merge) snowplow_incremental materialization
    upsert_date_key: RS/PG/Databricks only. Key used to limit the table scan
    partition_by: BQ only. Key used to limit table scan
    TODO: Add tests that change the granularity of the partition #}
 
-{{ 
+{{
   config(
     materialized='snowplow_incremental',
     unique_key='id',
@@ -13,13 +13,13 @@
       "data_type": "timestamp"
     }),
     tags=["requires_script"]
-  ) 
+  )
 }}
 
 with data as (
   select * from {{ ref('data_snowplow_incremental') }}
   {% if target.type == 'snowflake' %}
-    -- data set intentionally contains dupes. 
+    -- data set intentionally contains dupes.
     -- Snowflake merge will error if dupes occur. Removing for test
     where not (run = 1 and id = 2 and start_tstamp = '2021-03-03 00:00:00')
   {% endif %}
@@ -27,17 +27,17 @@ with data as (
 
 {% if snowplow_utils.snowplow_is_incremental() %}
 
-  select 
-    id, 
+  select
+    id,
     start_tstamp
 
   from data
   where run = 2
 
 {% else %}
-  
-  select 
-    id, 
+
+  select
+    id,
     start_tstamp
 
   from data
