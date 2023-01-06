5d6381f47ae89d967449d1f7a7c586f2f21081c3
Agnes Kiss <agnes@snowplowanalytics.com>
2023-01-06 14:04:03 +0000

    Add delimiter parameter to get_split_to_array (Close #106)

--- macros/utils/cross_db/get_split_to_array.sql
+++ macros/utils/cross_db/get_split_to_array.sql
@@ -2,18 +2,18 @@
  Takes care of harmonising cross-db functions that create an array out of a string.
  #}
 
-{%- macro get_split_to_array(string_column, column_prefix) -%}
-    {{ return(adapter.dispatch('get_split_to_array', 'snowplow_utils')(string_column, column_prefix)) }}
+{%- macro get_split_to_array(string_column, column_prefix, delimiter=',') -%}
+    {{ return(adapter.dispatch('get_split_to_array', 'snowplow_utils')(string_column, column_prefix, delimiter)) }}
 {%- endmacro -%}
 
-{% macro default__get_split_to_array(string_column, column_prefix) %}
-   split({{column_prefix}}.{{string_column}}, ',')
+{% macro default__get_split_to_array(string_column, column_prefix, delimiter=',') %}
+   split({{column_prefix}}.{{string_column}}, '{{delimiter}}')
 {% endmacro %}
 
-{% macro redshift__get_split_to_array(string_column, column_prefix) %}
-    split_to_array({{column_prefix}}.{{string_column}})
+{% macro redshift__get_split_to_array(string_column, column_prefix, delimiter=',') %}
+    split_to_array({{column_prefix}}.{{string_column}}, '{{delimiter}}')
 {% endmacro %}
 
-{% macro postgres__get_split_to_array(string_column, column_prefix) %}
-    string_to_array({{column_prefix}}.{{string_column}}, ',')
+{% macro postgres__get_split_to_array(string_column, column_prefix, delimiter=',') %}
+    string_to_array({{column_prefix}}.{{string_column}}, '{{delimiter}}')
 {% endmacro %}
