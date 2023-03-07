e00a543bc0e58b9b9aa5601c441b3908403c9649
Ryan Hill <r.l.hill128@gmail.com>
2023-03-07 10:38:57 +0000

    Update old materialization references

--- macros/materializations/snowplow_incremental/common/snowplow_is_incremental.sql
+++ macros/materializations/snowplow_incremental/common/snowplow_is_incremental.sql
@@ -4,6 +4,10 @@
   {% if not execute %}
     {{ return(False) }}
   {% else %}
+
+    {%- set error_message = "Warning: the `snowplow_is_incremental` macro is deprecated as is the materialization, and should be replaced with dbt's `is_incremental` materialization. It will be removed completely in a future version of the package." -%}
+    {%- do exceptions.warn(error_message) -%}
+
     {% set relation = adapter.get_relation(this.database, this.schema, this.table) %}
     {{ return(relation is not none
               and relation.type == 'table'
--- macros/utils/get_cluster_by.sql
+++ macros/utils/get_cluster_by.sql
@@ -1,6 +1,6 @@
 {% macro get_cluster_by(bigquery_cols=none, snowflake_cols=none) %}
 
-  {# {%- do exceptions.warn("Warning: the `get_cluster_by` macro is deprecated and will be removed in a future version of the package, please use `get_value_by_target_type` instead.") -%} #}
+  {%- do exceptions.warn("Warning: the `get_cluster_by` macro is deprecated and will be removed in a future version of the package, please use `get_value_by_target_type` instead.") -%}
 
 
   {% if target.type == 'bigquery' %}
