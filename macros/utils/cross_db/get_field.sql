6d378b39184df2f3c8efb5d796f52071eece4799
Ryan Hill <r.l.hill128@gmail.com>
2023-09-12 09:50:04 +0100

    Add multi-version support to get_field for bigquery

--- macros/utils/cross_db/get_field.sql
+++ macros/utils/cross_db/get_field.sql
@@ -5,27 +5,48 @@ and you may not use this file except in compliance with the Snowplow Community L
 You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
 #}
 
-{% macro get_field(column_name, field_name, table_alias = none, type = none, array_index = none) %}
-    {{ return(adapter.dispatch('get_field', 'snowplow_utils')(column_name, field_name, table_alias, type, array_index)) }}
+{% macro get_field(column_name, field_name, table_alias = none, type = none, array_index = none, relation = none) %}
+    {{ return(adapter.dispatch('get_field', 'snowplow_utils')(column_name, field_name, table_alias, type, array_index, relation)) }}
 {% endmacro %}
 
-{% macro bigquery__get_field(column_name, field_name, table_alias = none, type = none, array_index = none) %}
+{% macro bigquery__get_field(column_name, field_name, table_alias = none, type = none, array_index = none, relation = none) %}
+
+{% if '*' in column_name %}
+  {{ snowplow_utils.get_optional_fields(
+        enabled=true,
+        fields=[{'field': field_name, 'dtype': type or 'string' }],
+        col_prefix=column_name|replace('_*', ''),
+        relation=relation,
+        relation_alias=table_alias,
+        include_field_alias=false
+    ) }}
+
+{% else %}
 {%- if type -%}cast({%- endif -%}{%- if table_alias -%}{{table_alias}}.{%- endif -%}{{column_name}}{%- if array_index is not none -%}[SAFE_OFFSET({{array_index}})]{%- endif -%}.{{field_name}}{%- if type %} as {{type}}){%- endif -%}
+{% endif %}
 {% endmacro %}
 
-{% macro spark__get_field(column_name, field_name, table_alias = none, type = none, array_index = none) %}
-{%- if table_alias -%}{{table_alias}}.{%- endif -%}{{column_name}}{%- if array_index is not none -%}[{{array_index}}]{%- endif -%}.{{field_name}}{%- if type -%}::{{type}}{%- endif -%}
+{% macro spark__get_field(column_name, field_name, table_alias = none, type = none, array_index = none, relation = none) %}
+{% if '*' in column_name %}
+    {% do exceptions.raise_compiler_error('Wildcard schema versions are only supported for Bigquery, they are not supported for ' ~ target.type) %}
+{% else %}
+    {%- if table_alias -%}{{table_alias}}.{%- endif -%}{{column_name}}{%- if array_index is not none -%}[{{array_index}}]{%- endif -%}.{{field_name}}{%- if type -%}::{{type}}{%- endif -%}
+{% endif %}
 {% endmacro %}
 
-{% macro snowflake__get_field(column_name, field_name, table_alias = none, type = none, array_index = none) %}
-{%- if type is none and execute -%}
-{% do exceptions.warn("Warning: macro snowplow_utils.get_field is being used without a type provided, Snowflake will return a variant column in this case which is unlikely to be what you want.") %}
-{%- endif -%}
-{%- if table_alias -%}{{table_alias}}.{%- endif -%}{{column_name}}{%- if array_index is not none -%}[{{array_index}}]{%- endif -%}:{{field_name}}{%- if type -%}::{{type}}{%- endif -%}
+{% macro snowflake__get_field(column_name, field_name, table_alias = none, type = none, array_index = none, relation = none) %}
+{% if '*' in column_name %}
+    {% do exceptions.raise_compiler_error('Wildcard schema versions are only supported for Bigquery, they are not supported for ' ~ target.type) %}
+{% else %}
+    {%- if type is none and execute -%}
+        {% do exceptions.warn("Warning: macro snowplow_utils.get_field is being used without a type provided, Snowflake will return a variant column in this case which is unlikely to be what you want.") %}
+    {%- endif -%}
+    {%- if table_alias -%}{{table_alias}}.{%- endif -%}{{column_name}}{%- if array_index is not none -%}[{{array_index}}]{%- endif -%}:{{field_name}}{%- if type -%}::{{type}}{%- endif -%}
+{% endif %}
 {% endmacro %}
 
 
-{% macro default__get_field(column_name, field_name, table_alias = none, type = none, array_index = none) %}
+{% macro default__get_field(column_name, field_name, table_alias = none, type = none, array_index = none, relation = none) %}
 
 {% if execute %}
     {% do exceptions.raise_compiler_error('Macro get_field only supports Bigquery, Snowflake, Spark, and Databricks, it is not supported for ' ~ target.type) %}
