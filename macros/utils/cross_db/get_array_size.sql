692fbabe540677e7f889f69d0bc730101233394e
Agnes Kiss <agnes@snowplowanalytics.com>
2024-01-02 17:54:16 +0000

    Add get_array_size macro

---
+++ macros/utils/cross_db/get_array_size.sql
@@ -0,0 +1,30 @@
+{#
+Copyright (c) 2021-present Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Snowplow Community License Version 1.0,
+and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
+You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
+#}
+
+{#
+    Takes care of harmonising cross-db functions that create a string out of an array.
+#}
+
+{%- macro get_array_size(array_column) -%}
+    {{ return(adapter.dispatch('get_array_size', 'snowplow_utils')(array_column)) }}
+{%- endmacro -%}
+
+{% macro default__get_array_size(array_column) %}
+    array_size({{array_column}})
+{% endmacro %}
+
+{% macro bigquery__get_array_size(array_column) %}
+  array_length({{array_column}})
+{% endmacro %}
+
+{% macro postgres__get_array_size(array_column) %}
+    array_length({{array_column}})
+{% endmacro %}
+
+{% macro redshift__get_array_size(array_column) %}
+  get_array_length({{array_column}})
+{% endmacro %}
