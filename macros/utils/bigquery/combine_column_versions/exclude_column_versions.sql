e8719678611094d37b789016668e9626d17ab19d
Ryan Hill <r.l.hill128@gmail.com>
2024-06-10 12:36:57 +0100

    Extend functionality of combine column versions

--- macros/utils/bigquery/combine_column_versions/exclude_column_versions.sql
+++ macros/utils/bigquery/combine_column_versions/exclude_column_versions.sql
@@ -5,20 +5,30 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 {% macro exclude_column_versions(columns, exclude_versions) %}
+  {# 
+  columns: an array of column names
+  exclude_versions: an array of versions (more generically a suffix) to exclude columns that end with this value
+  returns: filtered array of columns that do not end with any of the exclude versions.
+  #}
   {% if not exclude_versions %}
     {{ return(columns) }}
   {% endif %}
 
   {%- set filtered_columns_by_version = [] -%}
+
+  {%- set columns_to_exclude = [] -%}
+
   {% for column in columns %}
   {# Remove columns that end with the version we want to exclude #}
     {% for version in exclude_versions %}
-      {% if not column.name.endswith(version) %}
-        {% do filtered_columns_by_version.append(column) %}
+      {% if column.name.endswith(version) %}
+        {% do columns_to_exclude.append(column) %}
       {% endif %}
     {% endfor %}
+    {% if not column in columns_to_exclude %}
+        {% do filtered_columns_by_version.append(column) %}
+    {% endif %}
   {% endfor %}
-
   {{ return(filtered_columns_by_version) }}
 
 {% endmacro %}
