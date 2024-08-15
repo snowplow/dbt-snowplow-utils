1d5d40e415c089b6420f0c62a59917f536431776
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-08-15 12:47:25 +0300

    Create spark_iceberg specific test for the future

--- integration_tests/.scripts/integration_tests.sh
+++ integration_tests/.scripts/integration_tests.sh
@@ -10,7 +10,7 @@ do
   esac
 done
 
-declare -a SUPPORTED_DATABASES=("bigquery" "databricks" "postgres" "redshift" "snowflake", "spark")
+declare -a SUPPORTED_DATABASES=("bigquery" "databricks" "postgres" "redshift" "snowflake", "spark_iceberg")
 
 # set to lower case
 DATABASE="$(echo $DATABASE | tr '[:upper:]' '[:lower:]')"
