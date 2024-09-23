4174cfc00fda0183f7cdf871767af5f78bdf8605
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-09-23 12:28:39 +0300

    fixes

--- macros/utils/post_ci_cleanup.sql
+++ macros/utils/post_ci_cleanup.sql
@@ -49,6 +49,42 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
 {% endmacro %}
 
 
+{% macro databricks__post_ci_cleanup(schema_pattern=target.schema) %}
+
+  {# Get all schemas with the target.schema prefix #}
+  {% set schemas = snowplow_utils.get_schemas_by_pattern(schema_pattern~'%') %}
+
+  {% if schemas|length %}
+
+    {%- if target.type in ['databricks'] -%}
+      {# Generate sql to drop all identified schemas #}
+      {% for schema in schemas -%}
+        {%- set drop_schema_sql -%}
+          DROP SCHEMA IF EXISTS {{schema}} CASCADE;
+        {%- endset -%}
+
+        {% do run_query(drop_schema_sql) %}
+
+      {% endfor %}
+
+    {%- else -%}
+      {# Generate sql to drop all identified schemas #}
+      {% set drop_schema_sql -%}
+
+        {% for schema in schemas -%}
+          DROP SCHEMA IF EXISTS {{schema}} CASCADE;
+        {% endfor %}
+
+      {%- endset %}
+
+      {# Drop schemas #}
+      {% do run_query(drop_schema_sql) %}
+
+    {%- endif -%}
+
+  {% endif %}
+
+{% endmacro %}
 
 {#
   Spark-specific implementation for post CI cleanup.
