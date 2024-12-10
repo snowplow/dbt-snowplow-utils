9129cf6defd44ee2a69761afaea6e469c24a0490
Agnes Kiss <agnes@snowplowanalytics.com>
2024-12-10 19:21:55 +0000

    Fix return_limits_from_model

---
+++ integration_tests/models/unit_tests/test_return_limits_from_model_macro/expected_return_limits_from_model_macro.sql
@@ -0,0 +1,25 @@
+{#
+Copyright (c) 2021-present Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Snowplow Personal and Academic License Version 1.0,
+and you may not use this file except in compliance with the Snowplow Personal and Academic License Version 1.0.
+You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
+#}
+
+
+SELECT 'dummy_model_empty' AS test_case, cast('9999-01-01 00:00:00.000' as {{ dbt.type_timestamp() }}) as lower_limit, cast('9999-01-02 00:00:00.000' as {{ dbt.type_timestamp() }}) AS upper_limit
+UNION ALL
+SELECT 'dummy_model_only_nulls', cast('9999-01-01 00:00:00.000' as {{ dbt.type_timestamp() }}) , cast('9999-01-02 00:00:00.000' as {{ dbt.type_timestamp() }})
+UNION ALL
+SELECT 'dummy_model_standard', cast('2024-12-01 00:00:00.000' as {{ dbt.type_timestamp() }}) , cast('2024-12-02 00:00:00.000' as {{ dbt.type_timestamp() }})
+UNION ALL
+SELECT 'dummy_model_empty_with_lower_output_true', cast('1999-01-01 00:00:00.000' as {{ dbt.type_timestamp() }}), cast('1999-01-02 00:00:00.000'  as {{ dbt.type_timestamp() }})
+UNION ALL
+SELECT 'dummy_model_empty_with_lower_output_false', cast('9999-01-01 00:00:00.000' as {{ dbt.type_timestamp() }}), cast('9999-01-02 00:00:00.000' as {{ dbt.type_timestamp() }})
+UNION ALL
+SELECT 'dummy_model_only_nulls_with_lower_output_true', cast('1999-01-01 00:00:00.000' as {{ dbt.type_timestamp() }}), cast('1999-01-02 00:00:00.000'  as {{ dbt.type_timestamp() }})
+UNION ALL
+SELECT 'dummy_model_only_nulls_with_lower_output_false', cast('9999-01-01 00:00:00.000' as {{ dbt.type_timestamp() }}), cast('9999-01-02 00:00:00.000' as {{ dbt.type_timestamp() }})
+UNION ALL
+SELECT 'dummy_model_standard_with_lower_output_true', cast('2024-12-01 00:00:00.000' as {{ dbt.type_timestamp() }}), cast('2024-12-02 00:00:00.000' as {{ dbt.type_timestamp() }})
+UNION ALL
+SELECT 'dummy_model_standard_with_lower_output_false', cast('2024-12-01 00:00:00.000' as {{ dbt.type_timestamp() }}), cast('2024-12-02 00:00:00.000' as {{ dbt.type_timestamp() }})
