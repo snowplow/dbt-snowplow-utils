aefe4f0d921878ffc151165f2623e8a1c577baeb
Agnes Kiss <agnes@snowplowanalytics.com>
2024-12-11 18:39:32 +0000

    Add unit tests for manifest

--- integration_tests/.scripts/unit_tests.sh
+++ integration_tests/.scripts/unit_tests.sh
@@ -26,14 +26,63 @@ fi
 
 for db in ${DATABASES[@]}; do
 
+  # Run dbt seed to set up the database, this prepares the ground for int tests that come after unit tests
+
+  echo "Snowplow unified unit tests: Seeding data"
+  eval "dbt seed --full-refresh --target $db" || exit 1;
+
     # In order to test this macro we need a model reference first and also a timestamp column which the macro takes the min and max of
     # We need to make sure that the correct result is returned even if the table is empty and whether they want the output to be a low or a high set date in that case
     # All in the models folder
     
-  if [[ $BRANCH == "release" || $BRANCH == "fix/return_limits" ]]; then
+  if [[ $BRANCH == "release" || $BRANCH == "utils_revamp" ]]; then
     echo "Snowplow-utils unit tests: Run test_return_limits_from_model_macro"
     eval "dbt run --select +test_return_limits_from_model_macro expected_return_limits_from_model_macro  --target $db --full-refresh" || exit 1;
-    eval "dbt test --select +test_return_limits_from_model_macro --store-failures --target $db" || exit 1;
+    eval "dbt test --select test_return_limits_from_model_macro --store-failures --target $db" || exit 1;
+  fi
+  
+    # This macro returns different queries for different states which will be used to create the base_new_event_limits table
+    # We need to make sure that the correct result is returned from this query depending on different inputs
+    # Inputs are given based on the get_incremental_manifest_status macro but we can just fake it as it returns an array 
+    # Input example: ['9999-01-01 00:00:00', '9999-01-01 00:00:00', 0, false]
+    # Inputs are read from a seed file
+  
+  if [[ $BRANCH == "release" || $BRANCH == "utils_revamp" ]]; then
+    echo "Snowplow-utils unit tests: Run test_get_run_limits_macro"
+    eval "dbt run --select test_get_run_limits_macro  --target $db --full-refresh" || exit 1;
+    eval "dbt test --select test_get_run_limits_macro --store-failures --target $db" || exit 1;
+  fi
+
+    # This macro returns different queries for different states which will be used to create the base_new_event_limits table
+    # We need to make sure that the correct result is returned from this query depending on different inputs
+    # Inputs are given based on the get_incremental_manifest_status macro but we can just fake it as it returns an array 
+    # Input example: ['9999-01-01 00:00:00', '9999-01-01 00:00:00', 0, 0, false]
+    # Inputs are read from a seed file
+  
+  if [[ $BRANCH == "release" || $BRANCH == "utils_revamp" ]]; then
+    echo "Snowplow-utils unit tests: Run test_get_run_limits_t_macro"
+    eval "dbt run --select test_get_run_limits_t_macro --target $db --full-refresh" || exit 1;
+    eval "dbt test --select test_get_run_limits_t_macro --store-failures --target $db" || exit 1;
+  fi
+  
+    # This macro returns returns the array: [min_last_success, max_last_success, models_matched_from_manifest, has_matched_all_models]
+    # Not too important to test, it is effectively returns a min/max/count from values in the manifest based on the models in the run
+    # Inputs are read from a seed file, we can selectively test the different inputs depending on the models in run array so no need for it to contain exact scenarios upfront
+  
+  if [[ $BRANCH == "release" || $BRANCH == "utils_revamp" ]]; then
+    echo "Snowplow-utils unit tests: Run test_get_incremental_manifest_status_macro"
+    eval "dbt run --select test_get_incremental_manifest_status_macro --target $db --full-refresh" || exit 1;
+    eval "dbt test --select test_get_incremental_manifest_status_macro --store-failures --target $db" || exit 1;
+  fi
+  
+    # This macro returns returns the array: [min_first_processed_load_tstamp, max_first_processed_load_tstamp, min_last_processed_load_tstamp, max_last_processed_load_tstamp, models_matched_from_manifest, sync_count, has_matched_all_models]
+    # Not too important to test, it is effectively returns a min/max/count from values in the manifest based on the models in the run
+    # Inputs are read from a seed file, we can selectively test the different inputs depending on the models in run array so no need for it to contain exact scenarios upfront
+  
+  if [[ $BRANCH == "release" || $BRANCH == "utils_revamp" ]]; then
+    echo "Snowplow-utils unit tests: Run test_get_incremental_manifest_status_t_macro"
+    eval "dbt run --select test_get_incremental_manifest_status_t_macro --target $db --full-refresh" || exit 1;
+    eval "dbt test --select test_get_incremental_manifest_status_t_macro --store-failures --target $db" || exit 1;
   fi
 
 done
