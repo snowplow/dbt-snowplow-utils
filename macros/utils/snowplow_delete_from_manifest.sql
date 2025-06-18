986d126f2f0be0f75209cd7f00f3c1bf139eebfd
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2025-06-18 10:34:40 +0300

    Updates

--- macros/utils/snowplow_delete_from_manifest.sql
+++ macros/utils/snowplow_delete_from_manifest.sql
@@ -45,14 +45,8 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   {%- endif -%}
 
   {% set delete_statement %}
-    {%- if target.type in ['databricks'] -%}
+    {%- if target.type in ['databricks', 'spark', 'duckdb'] -%}
       DELETE FROM {{ incremental_manifest_table }}
-      WHERE model IN ({{ snowplow_utils.print_list(matched_models) }});
-    {%- elif target.type in ['spark'] -%}
-      DELETE FROM {{ incremental_manifest_table }}
-      WHERE model IN ({{ snowplow_utils.print_list(matched_models) }});
-    {%- elif target.type in ['duckdb'] -%}
-      DELETE FROM {{ incremental_manifest_table }} 
       WHERE model IN ({{ snowplow_utils.print_list(matched_models) }});
     {%- else -%}
       -- We don't need transaction but Redshift needs commit statement while BQ does not. By using transaction we cover both.
