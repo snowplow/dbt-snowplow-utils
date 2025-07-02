ee173075185c2bbe24defced44e97b439e748a9a
Asad Manzoor <asad.manzoor@snowplowanalytics.com>
2025-07-02 11:46:06 +0100

    license_check macro receives var value and general clean up

--- macros/utils/license_check.sql
+++ macros/utils/license_check.sql
@@ -4,13 +4,15 @@ This program is licensed to you under the Snowplow Personal and Academic License
 and you may not use this file except in compliance with the Snowplow Personal and Academic License Version 1.0.
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
-{# Deletes specified models from the incremental_manifest table #}
-{% macro licence_check() %}
+{# Checks that user has accepted the Snowplow license terms #}
 
-  {% if not var('snowplow__license_accepted') %}
+{% macro license_check(license_acceptance) %}
+
+  {% if not license_acceptance %}
           {{ exceptions.raise_compiler_error(
             "🚫 Snowplow Error: License not accepted. Please set dbt_project.yml variable 'snowplow__license_accepted' to true in snowplow-utils to proceed."
       ) }}
+
   {% endif %}
 
 {% endmacro %}
