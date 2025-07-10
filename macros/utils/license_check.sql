f6090355f4075354e19ff931966793ab8320005b
Asad Manzoor <asad.manzoor@snowplowanalytics.com>
2025-07-10 10:35:55 +0100

    update license acceptance message

--- macros/utils/license_check.sql
+++ macros/utils/license_check.sql
@@ -10,7 +10,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
 
   {% if not license_acceptance %}
           {{ exceptions.raise_compiler_error(
-            "🚫 Snowplow Error: License not accepted. Please accept by setting dbt_project.yml variable 'snowplow__license_accepted' to true to proceed."
+            "🚫 Snowplow Error: To use this dbt package, you must accept the Snowplow Personal and Academic License or have a commercial agreement with Snowplow. See: https://docs.snowplow.io/personal-and-academic-license-1.0. To accept, set the  dbt_project.yml variable 'snowplow__license_accepted' to true."
       ) }}
 
   {% endif %}
