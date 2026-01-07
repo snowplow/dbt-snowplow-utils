ebc0c16561c6d9193f7e40ad39563394ec260b2b
Agnes Kiss <agnes@snowplowanalytics.com>
2026-01-07 19:26:14 +0000

    Silence warning for core users

--- macros/fusion_compat.sql
+++ macros/fusion_compat.sql
@@ -1,8 +1,10 @@
 {% macro config_meta_get(key, default=none) %}
-    {%- if config.get(key) != none -%}
+    {%- set meta_dict = config.get("meta", {}) -%}
+    
+    {%- if key in meta_dict -%}
+        {{ return(meta_dict.get(key)) }}
+    {%- elif config.get(key) != none -%}
         {{ return(config.get(key)) }}
-    {%- elif config.get("meta") != none and (key in config.get("meta", {}).keys()) -%}
-        {{ return(config.get("meta").get(key)) }}
     {%- else -%}
         {{ return(default) }}
     {%- endif -%}
@@ -13,10 +15,12 @@
     {# the first case is required to avoid errors #}
     {%- if config == {} -%}
         {{ return(none) }}
+    {# Check meta first to satisfy Fusion and stop Core 1.10+ lookup warnings #}
+    {%- elif config.get("meta") != none and key in config.get("meta", {}) -%}
+        {{ return(config.get("meta").get(key)) }}
+    {# Fallback to top-level if not in meta - for core #}
     {%- elif config.get(key) != none -%}
         {{ return(config.get(key)) }}
-    {%- elif config.get("meta") != none and (key in config.get("meta", {}).keys()) -%}
-        {{ return(config.get("meta").get(key)) }}
     {%- else -%}
         {% do exceptions.raise_compiler_error("Configuration '" ~ key ~ "' is required but was not found under config or meta (Fusion requires custom configuration under meta)") %}
     {%- endif -%}
